bc4c0b2d95b95fabd6f37b8ca72b50097c8500a3
Nathan Welch <nathanwelch_@hotmail.com>
2020-06-20 13:55:06 +1000

    Better OO
    * HumanPlayer and ComputerPlayer classes
    * implement PlayerInterface
    * computer selects random input

--- run.py
+++ run.py
@@ -1,36 +1,35 @@
-import numpy as np
+from numpy import zeros
+from random import randint
 
 COLUMNS = 7
 ROWS = 6
 
-PLAYER_ONE = 1
-PLAYER_TWO = 2
-
 
 class Board:
     def __init__(self):
-        self.game_board = np.zeros((ROWS, COLUMNS))
+        self.game_board = zeros((ROWS, COLUMNS))
 
     def print(self):
-        for row in self.game_board:
+        for row in self.game_board[::-1]:
             print("| " + " | ".join(str(int(c)) for c in row) + " |")
         print()
 
-    def drop_piece(self, position, player):
-        for row in range(ROWS-1, 0, -1):
+    def drop_piece(self, position: int, player: int):
+        for row in range(ROWS):
             if self.game_board[row][position] == 0:
                 self.game_board[row][position] = player
                 return row
         return -1
 
-    def val(self, row, column):
+    def val(self, row: int, column: int):
         try:
-            return int(self.game_board[row][column])
+            if row >= 0 and column >= 0:
+                return int(self.game_board[row][column])
         except IndexError:
             pass
-        return 0
+        return -1
 
-    def is_win(self, player, row, column, r_delta, c_delta):
+    def is_win(self, player: int, row: int, column: int, r_delta: int, c_delta: int):
         board_slice = []
         for d in range(-3, 4):
             board_slice.append(self.val(row + d*r_delta, column + d*c_delta))
@@ -44,46 +43,81 @@ class Board:
                 return True
         return False
 
-    def is_winner(self, player, row, column):
-        return (
-            self.is_win(player, row, column, 0, 1)
-            or self.is_win(player, row, column, 1, 0)
-            or self.is_win(player, row, column, 1, 1)
-            or self.is_win(player, row, column, -1, 1)
-        )
+    def is_winner(self, player: int, row: int, column: int):
+        if self.is_win(player, row, column, 0, 1):
+            return "horizontal"
+        if self.is_win(player, row, column, 1, 0):
+            return "vertical"
+        if self.is_win(player, row, column, 1, 1):
+            return "positive diagonal"
+        if self.is_win(player, row, column, -1, 1):
+            return "negative diagonal"
+        return ""
 
 
-def handle_move(turn):
-    player = PLAYER_ONE if turn % 2 == 0 else PLAYER_TWO
+class PlayerInterface:
+    def __init__(self, id: int):
+        self.id = id
 
-    position = ""
-    while position == "":
-        position = input("Player {p} to move, enter 1-7: ".format(p=player))
-    print()
+    def get_position(self, game_board: tuple):
+        raise NotImplementedError
 
-    position = int(position)
-    if position < 1:
-        position = 1
-    if position > COLUMNS:
-        position = COLUMNS
-    return position-1, player
+    def get_name(self):
+        raise NotImplementedError
+
+    def print_win_msg(self, win: str):
+        f = "{n} wins on the {w}!"
+        print(f.format(n=self.get_name(), w=win))
+
+
+class HumanPlayer(PlayerInterface):
+    def get_position(self, game_board: tuple):
+        pos = ""
+        while pos == "":
+            f = "{n} to move, enter 1-7: "
+            pos = input(f.format(n=self.get_name()))
+
+        position = int(pos)
+        if position < 1:
+            position = 1
+        if position > COLUMNS:
+            position = COLUMNS
+        return position-1
+
+    def get_name(self):
+        return "Player {p}".format(p=self.id)
+
+
+class ComputerPlayer(PlayerInterface):
+    def get_position(self, game_board: tuple):
+        # just return a random int for now
+        position = randint(1, COLUMNS)
+        print("{n} enters {p}".format(n=self.get_name(), p=position))
+        return position-1
+
+    def get_name(self):
+        return "Computer {p}".format(p=self.id)
 
 
 def run():
     turn = 0
     board = Board()
+    player_one = ComputerPlayer(1)
+    player_two = ComputerPlayer(2)
     playing = True
 
     while playing:
         board.print()
-        position, player = handle_move(turn)
+        player = player_one if turn % 2 == 0 else player_two
+        position = player.get_position(board.game_board)
 
-        row = board.drop_piece(position, player)
+        row = board.drop_piece(position, player.id)
         if not row == -1:
             turn += 1
-            if board.is_winner(player, row, position):
+            win = board.is_winner(player.id, row, position)
+            if not win == "":
                 playing = False
-                print("Player {p} wins!".format(p=player))
+                player.print_win_msg(win)
         else:
             print("That column is full, try again")
             print()
